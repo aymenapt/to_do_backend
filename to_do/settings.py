d0dfbf1919e3bade0dfcef86c7e64c81badb4b8a
ababsa aymen <aymenababsa1@gmail.com>
2022-08-11 15:22:48 +0100

    fourth commit

--- to_do/settings.py
+++ to_do/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 
 REST_FRAMEWORK = {
