1093107237432fbfcb20ee73e2f4d2ca22b707c6
ababsa aymen <aymenababsa1@gmail.com>
2022-08-12 00:36:11 +0100

    fifth commit

--- to_do/settings.py
+++ to_do/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1','.localhost', 'doyourtask.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','.localhost', 'dourtasks.herokuapp.com']
 
 
 # Application definition
